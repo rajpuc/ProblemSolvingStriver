1864a7692dd314a81300fd95174ceed8670f38c5
rajesh <ajoypal.raj@gmail.com>
2024-12-27 00:28:05 +0600

    c2: Sum of all divisors Approach 1 O(n*sqrt)

--- code.cpp
+++ code.cpp
@@ -1,5 +1,39 @@
+//Problem Link: https://www.naukri.com/code360/problems/sum-of-all-divisors_8360720?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SOLUTION
+
 #include<bits/stdc++.h>
 using namespace std;
+
+
+int calculate(int n){
+	// cout<<"Running for: "<<n<<endl;
+	// if(n<3) return n;
+
+	int sum = 0;
+	// cout<<"temp: "<<sqrt(n)<<endl;
+	for(int i=1; i <= sqrt(n); i++){
+		if(n%i == 0){
+			if(i==n/i) sum += i;
+			else sum+=i+(n/i);
+			// cout<<i<<'-'<<n/i<<'-'<<sum<<endl;
+		}
+	}
+
+	return sum;
+}
+
+
+
+int sumOfDivisors(int n){
+	int sum = 0;
+	while(n>0){
+		sum += calculate(n);
+		n--;
+	}
+	return sum;
+}
+
+
+
 int main(){
 
 #ifndef ONLINE_JUDGE
@@ -7,7 +41,18 @@ int main(){
     freopen("output.txt", "w", stdout);
 #endif
 
-    string x;
-    cin>>x;
-    cout<<"Hey" << x;
+// start writing code here:
+int n;
+cin>>n;
+
+cout<< sumOfDivisors(n)<<endl;
+
 }
+
+
+
+                            
+
+
+                            
+                        
